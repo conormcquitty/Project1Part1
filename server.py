535687c77340645a579530418bc6bde850f4ebd9
Zach <zdrever@ualberta.ca>
2017-02-08 12:36:44 -0700

    Added csv reading function to server.py and changed the digraph.py to AdjacencyGraph Class

--- server.py
+++ server.py
@@ -1,1 +1,51 @@
-Hello
+import Graph from digraph
+
+
+def read_city_graph(city_graph):
+    ''' Turns a comma seperated list into an UndirectedAdjacencyGraph
+
+    Args:
+        city_graph: filename with extension .csv that exists
+        in the same directory
+
+    Returns:
+        g: an UndirectedAdjacencyGraph
+    '''
+
+    #Read file into a list of tokens from the file.
+    with open(city_graph) as f:  # f = open('some.csv')
+        reader = csv.reader(f)
+        rows = []
+        for row in reader:  # reads a line, or "row"
+            rows += row  # list of strings
+    f.close()
+
+    vertices = []
+    edges = []
+    #Parse which lines include data about a vertice and
+    #which lines include data about an edge.
+    #Create two lists of vertices and edges.
+    for i in range(len(rows)):
+        if rows[i] == 'V':
+            vertices.append(int(rows[i+1]))
+        if rows[i] == 'E':
+            edges.append((int(rows[i+1]), int(rows[i+2])))
+        i+=4
+    #Construct the UndirectedAdjacencyGraph from the
+    #lists 'vertices' and 'edges'
+    g = UndirectedAdjacencyGraph()
+    for v in vertices:
+        if not g.is_vertex(v):
+            g.add_vertex(v)
+    for e in edges:
+        if not g.is_edge(e):
+            g.add_edge(e)
+    return g
+
+def main():
+
+
+
+
+if __name__ == "__main__":
+    main()
