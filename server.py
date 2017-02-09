4c8ece5e7414f15b1bb51651541d95a94d7360dd
conormcquitty <conormcquitty@hotmail.com>
2017-02-09 15:02:53 -0700

    Added the dictionaries for both latitude and longitude, and the street names. our function read_city_graph now returns the graph, the lat_lon attached to each vertice and the street_name attached to each edge.

--- server.py
+++ server.py
@@ -10,6 +10,8 @@ def read_city_graph(city_graph):
 
     Returns:
         g: an AdjacencyGraph
+        lat_lon: the latitude and longitude dictionary associated with the vertice key
+        street_name: the street name dictionary associated with the edge key
     '''
 
     #Read file into a list of tokens from the file.
@@ -22,25 +24,32 @@ def read_city_graph(city_graph):
 
     vertices = []
     edges = []
+    lat_lon = dict()
+    street_name = dict()
     #Parse which lines include data about a vertice and
     #which lines include data about an edge.
     #Create two lists of vertices and edges.
+    #Creates the dictionary lat_lon, where the vertice number is the key and the latitude and longitude are attched to the key
+    #Creates the dictionary street_name, where the edge start and end points tuple is the key, and the street name is attached to the key
     for i in range(len(rows)):
         if rows[i] == 'V':
             vertices.append(int(rows[i+1]))
+
+            lat_lon[int(rows[i+1])] = ((int(float(rows[i+2])*100000)),(int(float(rows[i+3])*100000)))
         if rows[i] == 'E':
             edges.append((int(rows[i+1]), int(rows[i+2])))
+            street_name[(int(rows[i+1]), int(rows[i+2]))] = (rows[i+3])
         i+=4
-    #Construct the UndirectedAdjacencyGraph from the
+    #Construct the AdjacencyGraph from the
     #lists 'vertices' and 'edges'
-    g = UndirectedAdjacencyGraph()
+    g = Graph()
     for v in vertices:
         if not g.is_vertex(v):
             g.add_vertex(v)
     for e in edges:
         if not g.is_edge(e):
             g.add_edge(e)
-    return g
+    return g, lat_lon, street_name
 
 if __name__ == "__main__":
-    
+    graph, lat_lon, street_name = read_city_graph("edmonton-roads-2.0.1.txt")
